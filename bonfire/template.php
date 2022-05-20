c27e0c40c1de3ff4076788e3e5655c5a772efb5d
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-05-20 02:07:40 +0100

    Update template.php

--- bonfire/template.php
+++ bonfire/template.php
@@ -2,12 +2,12 @@
 <html lang="en">
     <head>
         <script src="https://cdn.tailwindcss.com"></script>
-        <title><?php echo $error['code']; ?> - <?php echo $site['name']; ?></title>
+        <title><?php echo $code; ?> - <?php echo $site['name']; ?></title>
     </head>
     <body>
         <div class="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 mt-12">
             <h1 class="xl:text-4xl text-3xl my-2 text-center">
-                Error <?php echo $error['code']; ?><br>
+                Error <?php echo $code; ?><br>
                 <strong><?php echo $error['name']; ?></strong>
             </h1>
         </div>
@@ -20,6 +20,10 @@
                         <h2 class="xl:text-xl text-lg mb-4">
                             <?php echo $error['description']; ?>
                         </h2>
+                        <?php } if ($error['image'] != NULL) { ?>
+                        <p class="my-2">
+                            <img src="<?php echo $error['image']; ?>" class="mx-auto w-1/2 md:w-1/3 lg:w-1/4">
+                        </p>
                         <?php } if ($error['tip'] != NULL) { ?>
                         <h2 class="xl:text-xl text-lg mb-4 py-1 px-2 bg-blue-100 border-l-2 border-blue-500 rounded-r-md">
                             <?php echo $error['tip']; ?>
