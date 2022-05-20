74330106cfbd1fffc52a8aaaa58e9227ec1465a1
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-05-20 02:35:26 +0100

    Update template.php

--- bonfire/template.php
+++ bonfire/template.php
@@ -2,13 +2,13 @@
 <html lang="en">
     <head>
         <script src="https://cdn.tailwindcss.com"></script>
-        <title><?php echo $code; ?> - <?php echo $site['name']; ?></title>
+        <title><?= $code; ?> - <?= $site['name']; ?></title>
     </head>
     <body>
         <div class="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 mt-12">
             <h1 class="xl:text-4xl text-3xl my-2 text-center">
-                Error <?php echo $code; ?><br>
-                <strong><?php echo $error['name']; ?></strong>
+                Error <?= $code; ?><br>
+                <strong><?= $error['name']; ?></strong>
             </h1>
         </div>
 
@@ -18,19 +18,19 @@
                     <div class="px-6 py-6">
                         <?php if ($error['description'] != NULL) { ?>
                         <h2 class="xl:text-xl text-lg mb-4">
-                            <?php echo $error['description']; ?>
+                            <?= $error['description']; ?>
                         </h2>
                         <?php } if ($error['image'] != NULL) { ?>
                         <p class="my-2">
-                            <img src="<?php echo $error['image']; ?>" class="mx-auto w-1/2 md:w-1/3 lg:w-1/4">
+                            <img src="<?= $error['image']; ?>" class="mx-auto w-1/2 md:w-1/3 lg:w-1/4">
                         </p>
                         <?php } if ($error['tip'] != NULL) { ?>
                         <h2 class="xl:text-xl text-lg mb-4 py-1 px-2 bg-blue-100 border-l-2 border-blue-500 rounded-r-md">
-                            <?php echo $error['tip']; ?>
+                            <?= $error['tip']; ?>
                         </h2>
                         <?php } if ($site['support_contact'] != NULL) { ?>
                         <p>
-                            <?php echo $site['support_contact']; ?>
+                            <?= $site['support_contact']; ?>
                         </p>
                         <?php } ?>
                     </div>
